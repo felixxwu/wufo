4e76cc3e884eb7f8cfcca429c5078134b810b884
Felix Wu <fwu@shipserv.com>
2025-02-27 15:56:57 +0000

    Remove vite-plugin-singlefile

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), viteSingleFile()],
+  plugins: [preact()],
   resolve: {
     alias: {
       react: 'preact/compat',
